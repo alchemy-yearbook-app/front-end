583f2c97da9102666dcc66864be8fd44bb363594
Kevin Do <kevind0501@gmail.com>
2022-04-11 16:02:26 -0700

    pushing up dev branch

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,5 @@ import './App.css'; /* Global CSS */
 import styles from './App.module.css'; /* CSS Modules */
 
 export default function App() {
-  return (
-    <h1
-      className={`
-      bg-teal
-        text-3xl
-        text-center
-      text-white
-        font-bold
-        p-10
-        w-1/2
-        mx-auto
-        mt-10
-        ${styles.myCustomCssClass}
-      `}
-    >
-      Hello, World!
-    </h1>
-  );
+  return <div>Hello World</div>;
 }
