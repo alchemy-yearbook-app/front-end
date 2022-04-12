0b8c52e3e9dd1b11fe160d76c357748411e0f5ee
Mark Voltaire <markjvoltaire@gmail.com>
2022-04-12 13:24:12 -0400

    added more routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import styles from './App.module.css'; /* CSS Modules */
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './views/Home/Home';
 import Advice from './views/Advice/Advice';
+import Yearbook from './views/Yearbook/Yearbook';
 
 function App() {
   return (
@@ -14,7 +15,18 @@ function App() {
         <Route path="/advice">
           <Advice />
         </Route>
-        
+        <Route path="/sign-in">
+          <Auth />
+        </Route>
+        <Route path="/sign-up">
+          <Auth isSigningUp />
+        </Route>
+        <Route path="/yearbook">
+          <Yearbook />
+        </Route>
+        <Route path="/yearbook/create">
+          <Yearbook />
+        </Route>
       </Switch>
     </Router>
   );
