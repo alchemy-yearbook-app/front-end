0e467a711f51d927fd389a721032e766411aad49
Mark Voltaire <markjvoltaire@gmail.com>
2022-04-12 14:41:04 -0400

    added memorybook and yearbook component added

---
+++ src/components/Authform/Authform.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Authform() {
+  return <div>Authform</div>;
+}
---
+++ src/components/MemoryList/MemoryList.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Memorylist() {
+  return <div>Memorylist</div>;
+}
