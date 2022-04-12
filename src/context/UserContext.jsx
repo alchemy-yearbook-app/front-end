675f1eab298ac05589cd68680a135b1416c22457
Mark Voltaire <markjvoltaire@gmail.com>
2022-04-12 13:28:41 -0400

    added private routes and usercontext

---
+++ src/context/UserContext.jsx
@@ -0,0 +1,27 @@
+import { createContext, useContext, useMemo, useState } from 'react';
+import { getUser } from '../services/users';
+
+const UserContext = createContext();
+
+const UserProvider = ({ children }) => {
+  const currentUser = getUser();
+  const [user, setUser] = useState(
+    currentUser ? { id: currentUser.id, email: currentUser.email } : {}
+  );
+
+  const value = useMemo(() => ({ user, setUser }), [user]);
+
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+};
+
+const useUser = () => {
+  const context = useContext(UserContext);
+
+  if (context === undefined) {
+    throw new Error('useUser must be used within a UserProvider');
+  }
+
+  return context;
+};
+
+export { UserContext, UserProvider, useUser };
