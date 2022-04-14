c22a8d9d9032c718b15dc0ccb7eeacb59f83b8ce
mira-kine <mirak0419@gmail.com>
2022-04-14 11:54:10 -0700

    if current user is sign in is successful, redirects to profile

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,6 @@
 export const getCurrentUser = async () => {
   try {
-    const res = await fetch(`${process.env.API_URL}/api/v1/github/me`, {
+    const res = await fetch(`${process.env.API_URL}/api/v1/profile`, {
       credentials: 'include',
     });
     return res.json();
