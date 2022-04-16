af2e59146799cfd5ea1abcb8da063d272b36ad7d
Mark Voltaire <markjvoltaire@gmail.com>
2022-04-15 20:56:41 -0400

    changed fetch url to local host backend

--- src/components/YearbookList/YearbookList.jsx
+++ src/components/YearbookList/YearbookList.jsx
@@ -12,6 +12,7 @@ export default function YearbookList() {
       setUser(data);
       setLoading(false);
       console.log('data', data);
+      console.log('user', user);
     };
     fetchData();
   }, []);
--- src/services/yearbook.js
+++ src/services/yearbook.js
@@ -1,7 +1,9 @@
 export async function fetchGithubTeams() {
   const resp = await fetch(
-    'https://alchemy-yearbook-app.herokuapp.com/alchemycodelab/teams/students/teams'
+    // 'https://alchemy-yearbook-app.herokuapp.com/alchemycodelab/teams/students/teams'
+    'http://localhost:7890/api/v1/profile'
   );
   const data = await resp.json();
+  console.log('data', data);
   return data;
 }
