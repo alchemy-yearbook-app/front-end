0989b2242c335350f893c4bb9f9a7c8d11ad3a41
Kevin Do <kevind0501@gmail.com>
2022-04-18 15:18:05 -0700

    got the back end cohorts displayed!!!!

--- src/services/yearbook.js
+++ src/services/yearbook.js
@@ -1,8 +1,9 @@
 export async function fetchGithubTeams() {
   const resp = await fetch(
-    // 'https://alchemy-yearbook-app.herokuapp.com/alchemycodelab/teams/students/teams'
-    'http://localhost:7890/api/v1/profile'
+    'https://alchemy-yearbook-app.herokuapp.com/api/v1/orgs/alchemycodelab/teams/students/teams'
+    // 'http://localhost:7890/api/v1/profile'
   );
   const data = await resp.json();
+  console.log('data', data);
   return data;
 }
