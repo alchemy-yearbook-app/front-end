890087dec108fed51c72d4a2aee1f413916fce0b
Kevin Do <kevind0501@gmail.com>
2022-04-18 16:25:16 -0700

    refactored styling to yearbook list

--- src/components/YearbookList/YearbookList.jsx
+++ src/components/YearbookList/YearbookList.jsx
@@ -39,8 +39,21 @@ export default function YearbookList() {
             <div className="font-bold text-4xl uppercase text-center">
               {item.name}
             </div>
-            <div>
-              <YearbookCard user={user} />
+            <div className="w-full bg-gray-100 px-10 pt-10">
+              <div className="container mx-auto">
+                <div
+                  role="list"
+                  aria-label=""
+                  className="lg:flex md:flex sm:flex items-center xl:justify-between flex-wrap md:justify-around sm:justify-around lg:justify-around"
+                >
+                  <div
+                    role="listitem"
+                    className="xl:w-1/3 sm:w-3/4 md:w-2/5 relative mt-16 mb-32 sm:mb-24 xl:max-w-sm lg:w-2/5"
+                  >
+                    <YearbookCard user={user} />
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         ))}
