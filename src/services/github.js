eeb5efb9749cd52470dc7cceacdd0e9bbfd50459
Kevin Do <kevind0501@gmail.com>
2022-04-19 10:22:19 -0700

    changed heroku back end links

--- src/services/github.js
+++ src/services/github.js
@@ -1,6 +1,6 @@
 export async function fetchGithubUser() {
   const resp = await fetch(
-    'https://alchemy-yearbook-app.herokuapp.com/api/v1/profile'
+    'https://alchemy-yearbook-app-2.herokuapp.com/api/v1/profile'
   );
   const data = await resp.json();
   console.log('data', data);
