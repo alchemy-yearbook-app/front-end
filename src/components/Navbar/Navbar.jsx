522c00510a8a70af15011d9d537c3ccd449d31b3
Kevin Do <kevind0501@gmail.com>
2022-04-18 17:42:11 -0700

    styling navbar colors

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function Navbar() {
   return (
-    <nav class="bg-gray-200 shadow shadow-gray-300 w-100 px-8 md:px-auto">
+    <nav class="bg-darkerpurple shadow shadow-gray-300 w-100 px-8 md:px-auto mb-12">
       <div class="md:h-16 h-28 mx-auto md:px-4 container flex items-center justify-between flex-wrap md:flex-nowrap">
-        <div class="text-indigo-500 md:order-1">
+        <div class="text-white md:order-1">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             class="h-10 w-10"
@@ -20,7 +20,7 @@ export default function Navbar() {
             />
           </svg>
         </div>
-        <div class="text-gray-500 order-3 w-full md:w-auto md:order-2">
+        <div class="text-white order-3 w-full md:w-auto md:order-2">
           <ul class="flex font-semibold justify-between">
             <li class="md:px-4 md:py-2 text-indigo-500">
               <a href="#">Cohorts</a>
@@ -36,7 +36,7 @@ export default function Navbar() {
             </li>
           </ul>
         </div>
-        <div class="order-2 md:order-3">
+        <div class="order-2 md:order-3 text-white">
           <button class="px-4 py-2 bg-indigo-500 hover:bg-indigo-600 text-gray-50 rounded-xl flex items-center gap-2">
             <svg
               xmlns="http://www.w3.org/2000/svg"
