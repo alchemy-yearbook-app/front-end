b22db2e1e49d19509021b8427d9b25a2fc431af8
mira-kine <mirak0419@gmail.com>
2022-04-20 15:03:06 -0700

    user object coming through profileForm

--- src/components/YearbookList/YearbookList.jsx
+++ src/components/YearbookList/YearbookList.jsx
@@ -33,7 +33,7 @@ export default function YearbookList() {
   useEffect(() => {
     const fetchData = async () => {
       const data = await fetchYearbook();
-      console.log('data', data);
+      console.log('data2', data);
       setAlumni(data);
       setLoading(false);
     };
