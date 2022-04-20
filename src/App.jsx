55ff5abf4566ef2d4f47b2fadc7605370d78a709
Kevin Do <kevind0501@gmail.com>
2022-04-20 09:38:55 -0700

    unified app routes and changed styling of book

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,9 @@ import './App.css'; /* Global CSS */
 import styles from './App.module.css'; /* CSS Modules */
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './views/Home/Home';
-import Advice from './views/Advice/Advice';
 import Yearbook from './views/Yearbook/Yearbook';
 // import PrivateRoute from './components/PrivateRoute/PrivateRoute';
-// import Memorybook from './views/Memorybook/Memorybook';
+import Memorybook from './views/Memorybook/Memorybook';
 import About from './views/About/About';
 // import MemCreateEdit from './views/MemCreateEdit/MemCreateEdit';
 import ProfileCreateEdit from './views/ProfileCreateEdit/ProfileCreateEdit';
@@ -23,9 +22,6 @@ function App() {
           <Route exact path="/">
             <Home />
           </Route>
-          <Route exact path="/advice">
-            <Advice />
-          </Route>
           <Route exact path="/sign-in">
             <Auth />
           </Route>
@@ -38,16 +34,16 @@ function App() {
           <Route exact path="/profile/create">
             <ProfileCreateEdit />
           </Route>
-          <Route exact path="/alumadvice/">
+          <Route exact path="/advice/">
             <AlumAdvice />
           </Route>
           {/* <PrivateRoute exact path="/profile/:id/edit">
             <YearCreateEdit isEditing />
           </PrivateRoute> */}
-          {/* <PrivateRoute exact path="/memorybook">
+          <Route exact path="/memorybook">
             <Memorybook />
-          </PrivateRoute>
-          <PrivateRoute exact path="/memorybook/create">
+          </Route>
+          {/* <PrivateRoute exact path="/memorybook/create">
             <MemCreateEdit />
           </PrivateRoute>
           <PrivateRoute exact path="/memorybook/:id/edit">
--- src/views/Memorybook/Memorybook.jsx
+++ src/views/Memorybook/Memorybook.jsx
@@ -1,10 +1,72 @@
 import React from 'react';
-import Memorylist from '../../components/MemoryList/Memorylist';
+import './Memorybook.css';
 
 export default function Memorybook() {
   return (
     <div>
-      <Memorylist />
+      <div class="wrapper">
+        <div class="item">
+          <div class="polaroid">
+            <img src="https://emoji.slack-edge.com/T6FCZF1HR/potato-head/6c637bbe90391dfd.png" />
+            <div class="caption">Dan's Potato</div>
+          </div>
+        </div>
+
+        <div class="item">
+          <div class="polaroid">
+            <img src="https://i.ibb.co/mSCTRvQ/creeper.jpg" />
+            <div class="caption">Phoenix Nicholson</div>
+          </div>
+        </div>
+
+        <div class="item">
+          <div class="polaroid">
+            <img src="https://i.ibb.co/8XkQ9nw/IMG-10-EDF5-C11-CD4-1.jpg" />
+            <div class="caption">When the code</div>
+          </div>
+        </div>
+
+        <div class="item">
+          <div class="polaroid">
+            <img src="https://i.ibb.co/12KjvzC/Screen-Shot-2022-04-11-at-9-21-40-AM.png" />
+            <div class="caption">7 foot tall</div>
+          </div>
+        </div>
+
+        <div class="item">
+          <div class="polaroid">
+            <img src="https://i.ibb.co/2khXVR2/8806-revivedge.gif" />
+            <div class="caption">Deadge</div>
+          </div>
+        </div>
+        <div class="item">
+          <div class="polaroid">
+            <img src="https://i.ibb.co/12KjvzC/Screen-Shot-2022-04-11-at-9-21-40-AM.png" />
+            <div class="caption">7 foot tall</div>
+          </div>
+        </div>
+        <div class="item">
+          <div class="polaroid">
+            <img src="https://i.ibb.co/12KjvzC/Screen-Shot-2022-04-11-at-9-21-40-AM.png" />
+            <div class="caption">7 foot tall</div>
+          </div>
+        </div>
+        <div class="item">
+          <div class="polaroid">
+            <img src="https://i.ibb.co/12KjvzC/Screen-Shot-2022-04-11-at-9-21-40-AM.png" />
+            <div class="caption">7 foot tall</div>
+          </div>
+        </div>
+        <div class="item">
+          <div class="polaroid">
+            <div class="caption">
+              7 foot tall 7 foot tall 7 foot tall 7 foot tall 7 foot tall 7 foot
+              tall 7 foot tall 7 foot tall 7 foot tall 7 foot tall 7 foot tall 7
+              foot tall 7 foot tall 7 foot tall
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
