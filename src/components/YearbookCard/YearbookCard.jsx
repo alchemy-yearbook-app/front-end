289f9a6a93b5302ff43c75b042e0af9c2c17f83c
Kevin Do <kevind0501@gmail.com>
2022-04-21 15:20:29 -0700

    animation when hovered on github link

--- src/components/YearbookCard/YearbookCard.jsx
+++ src/components/YearbookCard/YearbookCard.jsx
@@ -25,7 +25,7 @@ export default function YearbookCard({ alumni }) {
                   {item.login}
                 </h1>
                 <div className="w-full flex justify-center pt-5 pb-5">
-                  <a href={item.htmlUrl} target="_blank" className="mx-5">
+                  <a href={item.htmlUrl} target="_blank" className="mx-5 ">
                     <div
                       aria-label="Github"
                       role="img"
@@ -41,7 +41,7 @@ export default function YearbookCard({ alumni }) {
                         strokeWidth="1.5"
                         strokeLinecap="round"
                         strokeLinejoin="round"
-                        className="feather feather-github"
+                        className="feather feather-github hover:rotate-12"
                       >
                         <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
                       </svg>
