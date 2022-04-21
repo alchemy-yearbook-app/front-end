9aba2dbc998796c03b3a7ec057b484a20cb11e6c
Kevin Do <kevind0501@gmail.com>
2022-04-21 09:46:41 -0700

    my cohort map styling

--- src/components/YearbookList/YearbookList.jsx
+++ src/components/YearbookList/YearbookList.jsx
@@ -46,24 +46,6 @@ export default function YearbookList() {
   return (
     <div>
       <div>
-        {/* {cohort.map((item) => (
-          <div key={item.id}>
-            <div className="font-bold text-4xl uppercase text-center">
-              {item.name}
-            </div>
-            <div className="w-full px-10 pt-10">
-              <div className="container mx-auto">
-                <div
-                  role="list"
-                  aria-label=""
-                  className="lg:flex md:flex sm:flex items-center xl:justify-between flex-wrap md:justify-around sm:justify-around lg:justify-around"
-                >
-                  <YearbookCard alumni={alumni} />
-                </div>
-              </div>
-            </div>
-          </div>
-        ))} */}
         <div className="font-bold text-4xl uppercase text-center">
           {alumni.name}
         </div>
@@ -74,12 +56,11 @@ export default function YearbookList() {
               aria-label=""
               className="lg:flex md:flex sm:flex items-center xl:justify-between flex-wrap md:justify-around sm:justify-around lg:justify-around"
             >
+              <YearbookCard alumni={alumni} />
               <div
                 role="listitem"
                 className="xl:w-1/3 sm:w-3/4 md:w-2/5 relative mt-16 mb-32 sm:mb-24 xl:max-w-sm lg:w-2/5"
-              >
-                <YearbookCard alumni={alumni} />
-              </div>
+              ></div>
             </div>
           </div>
         </div>
