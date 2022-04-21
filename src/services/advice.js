2305db4be60cbe9735df3cd815dfc4fd21549b6e
Mark Voltaire <markjvoltaire@gmail.com>
2022-04-21 15:24:14 -0400

    users can now create advice

--- src/services/advice.js
+++ src/services/advice.js
@@ -14,13 +14,12 @@ export const createAdvice = async ({ title, advice, alumniName, cohort }) => {
     }),
   });
   console.log('res.json()', res.json());
-  return res.json();
+  const data = await res.json;
+  return data;
 };
 
 export const getAdvice = async () => {
-  const resp = await fetch(
-    'https://alchemy-yearbook-app-2.herokuapp.com/api/v1/advice'
-  );
+  const resp = await fetch(`${process.env.API_URL}/api/v1/advice`);
   const data = await resp.json();
   console.log('resp', resp);
   return data;
