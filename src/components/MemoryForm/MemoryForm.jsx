0b752bb17fa240536c132c90429fb9c9470a4c1a
Kevin Do <kevind0501@gmail.com>
2022-04-21 15:02:02 -0700

    added to memory form for author

--- src/components/MemoryForm/MemoryForm.jsx
+++ src/components/MemoryForm/MemoryForm.jsx
@@ -18,17 +18,35 @@ export default function MemoryForm() {
                     placeholder="Enter Memory Title"
                   />
                 </section>
-
                 <section>
                   <label className="block font-bold mb-2 text-lg">
                     Image URL
+                  </label>
+                  <label className="block mb-2 text-sm">
+                    Right click on an image, copy image address online, or
+                    upload an image to{' '}
+                    <a
+                      href="https://imgbb.com/"
+                      target="_blank"
+                      className="text-darkteal"
+                    >
+                      https://imgbb.com/
+                    </a>
+                    , copy and paste the link into your browser, THEN right
+                    click image, copy image address and then paste URL below
                   </label>
                   <input
                     className="shadow appearance-none border rounded w-full py-1 px-1 mb-3 leading-tight focus:outline-none focus:shadow-outline"
                     placeholder="Enter Image Url"
                   />
                 </section>
-
+                <section>
+                  <label className="block font-bold mb-2 text-lg">Author</label>
+                  <input
+                    className="shadow appearance-none border rounded w-full py-1 px-1 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                    placeholder="Your Name"
+                  />
+                </section>
                 <button
                   type="submit"
                   className="bg-purple text-white hover:bg-darkerpurple py-0.3 font-bold rounded focus:outline-none focus:shadow-outline p-2 mt-8"
