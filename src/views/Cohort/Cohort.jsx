8c917d8c80898655301448e1527f3d909c0db2aa
mira-kine <mirak0419@gmail.com>
2022-04-21 20:44:13 -0700

    access token revoked

--- src/views/Cohort/Cohort.jsx
+++ src/views/Cohort/Cohort.jsx
@@ -1,39 +1,15 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { getCurrentUser } from '../../services/users';
-import {
-  fetchYearbookProfiles,
-  fetchYourCohort,
-} from '../../services/yearbook';
+import { fetchYearbookProfiles } from '../../services/yearbook';
 import CohortCard from '../../components/CohortCard/CohortCard';
 export default function Cohort() {
-  const [yourTeam, setYourTeam] = useState([]);
   const [yourTeamProfiles, setYourTeamProfiles] = useState([]);
-  const [user, setUser] = useState({});
   const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await getCurrentUser();
-      setUser(data);
-      setLoading(false);
-    };
-    fetchData();
-  }, []);
 
   useEffect(() => {
     const fetchData = async () => {
       const data1 = await fetchYearbookProfiles();
       setYourTeamProfiles(data1);
-    };
-    setLoading(false);
-    fetchData();
-  }, []);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await fetchYourCohort();
-      setYourTeam(data);
     };
     setLoading(false);
     fetchData();
@@ -57,10 +33,7 @@ export default function Cohort() {
                 aria-label=""
                 className="lg:flex md:flex sm:flex items-center xl:justify-between flex-wrap md:justify-around sm:justify-around lg:justify-around"
               >
-                <CohortCard
-                  yourTeam={yourTeam}
-                  yourTeamProfiles={yourTeamProfiles}
-                />
+                <CohortCard yourTeamProfiles={yourTeamProfiles} />
                 <div
                   role="listitem"
                   className="xl:w-1/3 sm:w-3/4 md:w-2/5 relative mt-16 mb-32 sm:mb-24 xl:max-w-sm lg:w-2/5"
