bded9e866d807fd74cb39c99b5aba037c57d97b0
mira-kine <mirak0419@gmail.com>
2022-04-22 09:13:23 -0700

    create profile

--- src/views/Profile/Profile.jsx
+++ src/views/Profile/Profile.jsx
@@ -15,41 +15,37 @@ export default function Profile() {
     history.push(`/profile/create`);
   };
 
-  const handleEdit = () => {
-    history.push(`/profile/${profile.id}/edit`);
-  };
+  // const handleEdit = () => {
+  //   history.push(`/profile/${profile.id}/edit`);
+  // };
 
   const handleBack = () => {
     history.push('/yearbook/');
   };
 
-  // if (loading) {
-  //   return <div></div>;
-  // }
+  if (loading) {
+    return <div></div>;
+  }
 
   return (
     <div>
       <div className="flex justify-center">
         <div className="bg-white rounded-2xl border shadow-xl p-10 max-w-lg">
           <div className="flex flex-col items-center space-y-4">
-            {!profile ? (
-              <>
-                <h1 className="font-bold text-3xl text-gray-700 w-4/6 text-center">
-                  Welcome!
-                </h1>
-                <p className="text-xl text-gray-500 text-center w-5/6">
-                  Please click below to enter your profile into the Alchemy
-                  Yearbook!
-                </p>
-                <button
-                  onClick={handleCreate}
-                  className="bg-purple text-white rounded-md font-semibold px-4 py-3 w-full"
-                >
-                  Create Profile
-                </button>
-              </>
-            ) : (
-              <>
+            <h1 className="font-bold text-3xl text-gray-700 w-4/6 text-center">
+              Welcome!
+            </h1>
+            <p className="text-xl text-gray-500 text-center w-5/6">
+              Please click below to enter your profile into the Alchemy
+              Yearbook!
+            </p>
+            <button
+              onClick={handleCreate}
+              className="bg-purple text-white rounded-md font-semibold px-4 py-3 w-full"
+            >
+              Create Profile
+            </button>
+            {/* <>
                 <h1 className="font-bold text-3xl text-gray-700 w-4/6 text-center">
                   Welcome back!
                 </h1>
@@ -63,7 +59,7 @@ export default function Profile() {
                   Edit Profile
                 </button>
               </>
-            )}
+           */}
             <button onClick={handleBack} className="text-darkpurple">
               Back to Yearbook
             </button>
